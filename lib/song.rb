b2bc5093b9442b77138927b53b0be465df1419d9
Trevor Russell <truss.42@gmail.com>
2018-06-18 21:59:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,18 +2,25 @@ class Song
   
   @@song_count = 0
   @@genres = []
+  @@artists = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@song_count += 1
-    @@genre << genre
+    @@genres << genre
+    @@artists << artist unless  @@artists.inculde?(artist)
   end
   
   def self.count 
     @@song_count
   end
+  
+  def artists
+    @@artists
+  end
+  
   
   
   
